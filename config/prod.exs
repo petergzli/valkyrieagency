3139b1cafa7157ec1d62a147f3732f020a35053c
Peter Li <petergzli@gmail.com>
2017-12-23 09:42:23 -0800

    valkyireagency adjustment

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :valkyrieagency, Valkyrieagency.Endpoint,
   http: [port: 8888],
-  url: [host: "example.com", port: 80],
+  url: [host: "valkyrieagency.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :phoenix, :serve_endpoints, true
